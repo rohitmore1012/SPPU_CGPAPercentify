06b90ddc9053168cdad42c22aacc01fb1b265a93
Rohit More <72143589+rohitmore1012@users.noreply.github.com>
2023-08-25 04:06:51 +0530

    Update SPPUCGPAPercentify.py

--- SPPUCGPAPercentify.py
+++ SPPUCGPAPercentify.py
@@ -2,6 +2,14 @@ import streamlit as st
 import streamlit.components.v1 as components
 from streamlit_option_menu import option_menu  # select_options library
 
+
+# Set page configuration to expand the sidebar by default
+st.set_page_config(initial_sidebar_state="expanded")
+
+# Initialize a state variable to control sidebar state
+collapse_sidebar = False
+
+
 # Function to determine grade based on CGPA
 
 
@@ -223,6 +231,17 @@ st.sidebar.write('''# SPPU CGPAPercentify ''')
 with st.sidebar:
     selected = option_menu("Navigation", ["Home", "CGPA to Percentage", "SGPA to Percentage",
                            "Grade Prediction", "Semester-wise CGPA and Percentage Calculation", "About"])
+
+    # After handling the option selection, change the state variable to collapse the sidebar
+    collapse_sidebar = True
+
+# If the state variable is True, collapse the sidebar
+if collapse_sidebar:
+    st.markdown(
+        '<style>div.sidebar-closed { display: none }</style>', unsafe_allow_html=True)
+    st.sidebar.markdown(
+        '<style>div[aria-expanded="false"] { margin-left: -40px; }</style>', unsafe_allow_html=True)
+
 
 if selected == "Home":
     st.markdown(
